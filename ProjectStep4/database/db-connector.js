f77cbdd91f867a3e96cb42e9ca4eceac5137d844
Socramf97 <91354056+Socramf97@users.noreply.github.com>
2024-11-21 14:09:32 -0800

    Privacy Change
    Updated to prevent credentials from being shown

--- ProjectStep4/database/db-connector.js
+++ ProjectStep4/database/db-connector.js
@@ -1,13 +1,16 @@
+// Load environment variables from .env file
+require('dotenv').config();
+
 // Get an instance of mysql we can use in the app
 var mysql = require('mysql')
 
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_fabiancm',
-    password        : 'ZhkOkpZ7XUyG',
-    database        : 'cs340_fabiancm'
+    host            : process.env.DB_HOST,
+    user            : process.env.DB_USER,
+    password        : process.env.DB_PASSWORD,
+    database        : process.env.DB_DATABASE
 })
 
 // Export it for use in our applicaiton
