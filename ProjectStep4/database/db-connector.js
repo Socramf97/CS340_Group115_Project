bda9053939343c411bbcf351febe87c3f7306c49
Socramf97 <91354056+Socramf97@users.noreply.github.com>
2024-11-21 22:12:01 -0800

    Added CRD Functionality, Update In progress
    User is able to Create, Read and Delete, update is a work in progress

--- ProjectStep4/database/db-connector.js
+++ ProjectStep4/database/db-connector.js
@@ -1,16 +1,13 @@
-// Load environment variables from .env file
-require('dotenv').config();
-
 // Get an instance of mysql we can use in the app
 var mysql = require('mysql')
 
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : process.env.DB_HOST,
-    user            : process.env.DB_USER,
-    password        : process.env.DB_PASSWORD,
-    database        : process.env.DB_DATABASE
+    host            : 'classmysql.engr.oregonstate.edu',
+    user            : 'cs340_fabiancm',
+    password        : 'ZhkOkpZ7XUyG',
+    database        : 'cs340_fabiancm'
 })
 
 // Export it for use in our applicaiton
